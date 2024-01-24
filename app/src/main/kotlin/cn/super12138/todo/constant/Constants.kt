8c22d7ac5f94802704d2d1f950cb85fc2281e806
Super12138 <70494801+Super12138@users.noreply.github.com>
2024-01-24 14:56:23 +0800

    Clean code

--- app/src/main/kotlin/cn/super12138/todo/constant/Constants.kt
+++ app/src/main/kotlin/cn/super12138/todo/constant/Constants.kt
@@ -1,10 +1,6 @@
 package cn.super12138.todo.constant
 
 object Constants {
-    const val DB_NAME = "ToDo.db"
-    const val DB_VERSION = 1
-    const val TABLE_NAME = "ToDo"
-
     const val AUTHOR_GITHUB_URL = "https://github.com/Super12138/"
     const val REPO_GITHUB_URL = "https://github.com/Super12138/ToDo"
     const val UPDATE_URL = "https://github.com/Super12138/ToDo/releases"
