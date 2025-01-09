2d7b3edf15af504e25f3c050219d8c1e77f67922
Super12138 <70494801+Super12138@users.noreply.github.com>
2025-01-09 21:50:44 +0800

    开始使用 Jetpack Compose 对待办重构

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,51 +1,28 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.aboutlibraries)
 }
-ksp {
-    arg("room.schemaLocation", "$projectDir/schemas")
-}
-
-val baseVersionName = "1.0.6"
-val commitHash by lazy { "git rev-parse --short HEAD".exec() }
-val verCode by lazy { "git rev-list --count HEAD".exec().toInt() }
 
 android {
     namespace = "cn.super12138.todo"
     compileSdk = 35
 
-    // 获取 Release 签名
-    val releaseSigning = if (project.hasProperty("releaseStoreFile")) {
-        signingConfigs.create("release") {
-            storeFile = File(project.properties["releaseStoreFile"] as String)
-            storePassword = project.properties["releaseStorePassword"] as String
-            keyAlias = project.properties["releaseKeyAlias"] as String
-            keyPassword = project.properties["releaseKeyPassword"] as String
-        }
-    } else {
-        signingConfigs.getByName("debug")
-    }
-
     defaultConfig {
         applicationId = "cn.super12138.todo"
         minSdk = 24
         targetSdk = 35
-        versionCode = verCode
-        versionName = "${baseVersionName}-${commitHash}"
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        base.archivesName.set("todo-${baseVersionName}")
     }
 
     buildTypes {
-        all {
-            signingConfig = releaseSigning
-        }
         release {
             isMinifyEnabled = true
-            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -53,52 +30,54 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_18
-        targetCompatibility = JavaVersion.VERSION_18
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "18"
+        jvmTarget = "21"
     }
     buildFeatures {
-        viewBinding = true
+        compose = true
     }
 }
 
 dependencies {
 
     // Android X
-    implementation(libs.androidx.core)
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.core.core.splashscreen)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.activity.ktx)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.fragment)
-    implementation(libs.androidx.fragment.ktx)
-    implementation(libs.androidx.recyclerview)
-    implementation(libs.androidx.lifecycle.viewmodel)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.preference)
-    implementation(libs.androidx.preference.ktx)
-    implementation(libs.androidx.security)
-    // Material Design
-    implementation(libs.material)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.compose.runtime.livedata)
+    // implementation(libs.androidx.security)
+    // Compose
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.animation)
+    implementation(libs.androidx.navigation)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.android)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icon.core)
+    implementation(libs.androidx.material.icon.extended)
+    // About Libraries
+    implementation(libs.aboutlibraries.core)
+    implementation(libs.aboutlibraries.compose)
+    // Kotlin Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
     // Room
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     annotationProcessor(libs.androidx.room.compiler)
     ksp(libs.androidx.room.compiler)
-    // FastScroll
-    implementation(project(":fastscroll"))
     // Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
-
-fun String.exec(): String = exec(this)
-
-fun Project.exec(command: String): String = providers.exec {
-    commandLine(command.split(" "))
-}.standardOutput.asText.get().trim()
