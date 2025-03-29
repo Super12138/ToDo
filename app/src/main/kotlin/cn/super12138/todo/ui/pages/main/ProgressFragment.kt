f5ed806d8be6bdfa9f92cc54d3a6efd11292a4ab
Super12138 <70494801+Super12138@users.noreply.github.com>
2025-03-29 20:34:29 +0800

    refactor: 组件化进度条

--- app/src/main/kotlin/cn/super12138/todo/ui/pages/main/ProgressFragment.kt
+++ app/src/main/kotlin/cn/super12138/todo/ui/pages/main/ProgressFragment.kt
@@ -1,23 +1,20 @@
 package cn.super12138.todo.ui.pages.main
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import cn.super12138.todo.R
+import cn.super12138.todo.ui.components.AnimatedCircularProgressIndicator
 
 @Composable
 fun ProgressFragment(
@@ -32,17 +29,13 @@ fun ProgressFragment(
         // 没有任务时
         0f
     }
-    val animatedProgress by animateFloatAsState(
-        targetValue = progress,
-        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec,
-        label = "To Do Progress"
-    )
+
     Box(
         modifier = modifier,
         contentAlignment = Alignment.Center
     ) {
-        CircularProgressIndicator(
-            progress = { animatedProgress },
+        AnimatedCircularProgressIndicator(
+            progress = progress,
             strokeWidth = 10.dp,
             gapSize = 10.dp,
             modifier = Modifier.size(175.dp)
