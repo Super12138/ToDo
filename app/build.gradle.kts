5911b9e67f2c795d05109f3ecc3a1ad4cb41cdd6
Super12138 <70494801+Super12138@users.noreply.github.com>
2025-04-20 20:31:35 +0800

    feat: 优化崩溃页面的显示效果

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         applicationId = "cn.super12138.todo"
         minSdk = 24
         targetSdk = 36
-        versionCode = 633
+        versionCode = 634
         versionName = "2.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
