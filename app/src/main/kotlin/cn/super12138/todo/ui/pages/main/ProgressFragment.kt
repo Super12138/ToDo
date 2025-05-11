c4a7930cfda1973adde73ecdf92aec653942e54e
Super12138 <70494801+Super12138@users.noreply.github.com>
2025-05-11 13:49:15 +0800

    fix: 无障碍描述类型错误

--- app/src/main/kotlin/cn/super12138/todo/ui/pages/main/ProgressFragment.kt
+++ app/src/main/kotlin/cn/super12138/todo/ui/pages/main/ProgressFragment.kt
@@ -10,9 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.clearAndSetSemantics
-import androidx.compose.ui.semantics.stateDescription
+import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import cn.super12138.todo.R
@@ -24,23 +25,14 @@ fun ProgressFragment(
     completedTasks: Int,
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
+
     val remainTasks = totalTasks - completedTasks
     val progress = if (totalTasks != 0) {
         completedTasks / totalTasks.toFloat()
     } else {
         // 没有任务时
         0f
-    }
-
-    val progressDescription = if (totalTasks != 0) {
-        stringResource(
-            R.string.accessibility_progress_tasks,
-            totalTasks,
-            completedTasks,
-            remainTasks
-        )
-    } else {
-        stringResource(R.string.accessibility_progress_no_tasks)
     }
 
     Box(
@@ -60,7 +52,16 @@ fun ProgressFragment(
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.clearAndSetSemantics {
-                    stateDescription = progressDescription
+                    contentDescription = if (totalTasks != 0) {
+                        context.getString(
+                            R.string.accessibility_progress_tasks,
+                            totalTasks,
+                            completedTasks,
+                            remainTasks
+                        )
+                    } else {
+                        context.getString(R.string.accessibility_progress_no_tasks)
+                    }
                 }
             ) {
                 Text(
