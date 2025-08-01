b92a62e57f726d037c0d92b8ea45f6b3c4f4c3ea
Super12138 <70494801+Super12138@users.noreply.github.com>
2025-08-01 11:15:11 +0800

    feat: 支持在分类管理页面修改已有分类

--- app/src/main/kotlin/cn/super12138/todo/ui/pages/settings/components/category/CategoryItem.kt
+++ app/src/main/kotlin/cn/super12138/todo/ui/pages/settings/components/category/CategoryItem.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.sp
 import cn.super12138.todo.R
 import cn.super12138.todo.ui.TodoDefaults
 import cn.super12138.todo.utils.VibrationUtils
@@ -27,6 +26,7 @@ import cn.super12138.todo.utils.VibrationUtils
 fun CategoryItem(
     modifier: Modifier = Modifier,
     name: String,
+    onClick: (String) -> Unit = {},
     onDelete: (String) -> Unit = {}
 ) {
     val view = LocalView.current
@@ -35,6 +35,12 @@ fun CategoryItem(
             .fillMaxWidth()
             .wrapContentHeight()
             .clip(MaterialTheme.shapes.large)
+            .clickable(
+                onClick = {
+                    VibrationUtils.performHapticFeedback(view)
+                    onClick(name)
+                }
+            )
             .padding(
                 horizontal = TodoDefaults.settingsItemHorizontalPadding,
                 vertical = TodoDefaults.settingsItemVerticalPadding / 2
