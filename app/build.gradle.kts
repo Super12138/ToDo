25ed69391277d2b37bd0762ec176dce290387eef
Super12138 <70494801+Super12138@users.noreply.github.com>
2026-08-26 11:28:52 +0800

    发布3.5.0版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "cn.super12138.todo"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1242
-        versionName = "3.4.0"
+        versionCode = 1243
+        versionName = "3.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
